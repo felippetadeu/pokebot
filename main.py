8a67e9e9016c5a074d08d2adba5f731737568755
Felippe Silva <felippe_tadeu@hotmail.com>
2022-10-31 23:58:58 -0300

    commit inicial

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.entities.trainer import Trainer
 
 async def main():
     await Setup.start(True)
+    
 
     trainer = await Trainer.create('18304u1=123-41234', 'Felippe', 4)
     trainer.pokemons[0].gain_xp(5000)
